6a24e8faccf1a0f49809cdbada26a9702665d5d2
Naitian Zhou <nzhou2011@gmail.com>
2018-05-02 15:35:38 -0400

    Add stuff for Alex Gurung

--- script.js
+++ script.js
@@ -1,19 +1,19 @@
 var config = {
-    apiKey: "AIzaSyD2OpXee9TD17oKFORJJGPq51b5ZzFBFL8",
-    authDomain: "techstrav-signage.firebaseapp.com",
-    databaseURL: "https://techstrav-signage.firebaseio.com",
-    projectId: "techstrav-signage",
-    storageBucket: "techstrav-signage.appspot.com",
-    messagingSenderId: "492385124811"
+    apiKey: 'AIzaSyD2OpXee9TD17oKFORJJGPq51b5ZzFBFL8',
+    authDomain: 'techstrav-signage.firebaseapp.com',
+    databaseURL: 'https://techstrav-signage.firebaseio.com',
+    projectId: 'techstrav-signage',
+    storageBucket: 'techstrav-signage.appspot.com',
+    messagingSenderId: '492385124811'
 };
 firebase.initializeApp(config);
 
 // Get a reference to the database service
 var database = firebase.database();
-var ref = database.ref("testAnnouncements");
-ref.on("value", function (snapshot) {
+var ref = database.ref('testAnnouncements');
+ref.on('value', function (snapshot) {
     events = snapshot.val();
     for (var event in events) {
-        console.log(events);
+        console.log(event);
     }
 });
