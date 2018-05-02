46b8b9dc3c83668a08eaf7a4a12cd03ffc744ac3
alex-gurung <aag1234@gmail.com>
2018-05-02 15:42:07 -0400

    better json iteration

--- script.js
+++ script.js
@@ -13,7 +13,13 @@ var database = firebase.database();
 var ref = database.ref('testAnnouncements');
 ref.on('value', function (snapshot) {
     events = snapshot.val();
-    for (var event in events) {
-        console.log(event);
+    for (var i = 0; i < events.length; i++) {
+        event = events[i]
+        if (event.title) {
+            console.log(event.title)
+        }
+        if (event.content){
+            console.log(event.content)
+        }
     }
 });
