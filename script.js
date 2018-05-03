3f52f069e430e543ac3511a26532ea997fc7132a
Alex Suh <coolcatvas@gmail.com>
2018-05-02 22:02:54 -0400

    Changed double quotes back to single quotes

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 console.log("Ready");
 
 var config = {
-    apiKey: "AIzaSyD2OpXee9TD17oKFORJJGPq51b5ZzFBFL8",
-    authDomain: "techstrav-signage.firebaseapp.com",
-    databaseURL: "https://techstrav-signage.firebaseio.com",
-    projectId: "techstrav-signage",
-    storageBucket: "techstrav-signage.appspot.com",
-    messagingSenderId: "492385124811"
+    apiKey: 'AIzaSyD2OpXee9TD17oKFORJJGPq51b5ZzFBFL8',
+    authDomain: 'techstrav-signage.firebaseapp.com',
+    databaseURL: 'https://techstrav-signage.firebaseio.com',
+    projectId: 'techstrav-signage',
+    storageBucket: 'techstrav-signage.appspot.com',
+    messagingSenderId: '492385124811';
 };
 firebase.initializeApp(config);
 
@@ -16,20 +16,20 @@ function card(announcement) {
     if (!announcement.public) {
         return;
     }
-    let template = document.querySelector(".announcement-card-template").innerText;
-    let html = template.replace("%%%TITLE%%%", announcement.title);
-    html = html.replace("%%%CONTENT%%%", announcement.content);
-    let elem = document.createElement("div");
+    let template = document.querySelector('.announcement-card-template').innerText;
+    let html = template.replace('%%%TITLE%%%', announcement.title);
+    html = html.replace('%%%CONTENT%%%', announcement.content);
+    let elem = document.createElement('div');
     elem.innerHTML = html;
-    document.querySelector(".list").append(elem);
+    document.querySelector('.list').append(elem);
 }
 
 // Get a reference to the database service
 var database = firebase.database();
-var ref = database.ref("testAnnouncements");
-ref.on("value", function (snapshot) {
+var ref = database.ref('testAnnouncements');
+ref.on('value', function (snapshot) {
     events = snapshot.val();
-    document.querySelector(".list").innerHTML = "";
+    document.querySelector('.list').innerHTML = '';
     for (var i = 0; i < events.length; i++) {
         event = events[i];
         if (event.title) {
